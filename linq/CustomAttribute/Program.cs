264de8642aad19fa13e02bc1e5cc4523f70ec323
stpriyanka <stpriyanka2011@gmail.com>
2017-03-02 17:07:48 +0100

    Implement custom attribute in term of Billogram adress properties required fields

---
+++ linq/CustomAttribute/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomAttribute
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var test = new TestObj()
+			{
+				Name = "foo!",
+				PersonalNumber = "910112-2449",
+				Gender = null,
+			};
+			
+			IfSpecified.PerformCheck(test);
+
+			//var obj = new Customer();
+			//obj.CustomerCode = "12345678901";
+
+			//if (obj.CustomerCode != null)
+			//{
+			//	CheckFirst(obj);
+
+			//	Console.WriteLine("done");
+			//}
+		}
+
+
+		private static void CheckFirst(Customer obj)
+		{
+			Type objtype = obj.GetType();
+
+			// Loop through all properties                           //props  of Customer
+			foreach (PropertyInfo p in objtype.GetProperties())
+			{
+				// for every property loop through all attributes    // attributes of each prop 
+				foreach (Attribute a in p.GetCustomAttributes(false))
+				{
+					Check c = (Check)a;
+					if (p.Name == "CustomerCode")
+					{
+						// Do the length check and and raise exception accordingly
+						if (obj.CustomerCode.Length > c.MaxLength)
+						{
+							throw new Exception(" Max length issues ");
+						}
+
+					}
+				}
+			}
+		}
+	}
+
+
+	[AttributeUsage(AttributeTargets.Property)]
+	public class Check : Attribute
+	{
+		public int MaxLength { get; set; }
+		//public string CompareLength { get; set; }
+	}
+
+
+	public class Customer
+	{
+		[Check(MaxLength = 5)]
+		public string CustomerCode { get; set; }
+		public string Title { get; set; }
+	}
+}
---
+++ linq/CustomAttribute/RequiredIfAttribute.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Build.Framework;
+
+namespace CustomAttribute
+{
+	//public class Program1
+	//{
+	//	private static void Main1(string[] args)
+	//	{
+	//		var test = new TestObj()
+	//		{
+	//			Name = "foo!",
+	//			Gender = new object()
+	//		};
+	//		Required.PerformCheck(test);
+	//	}
+	//}
+
+	public class TestObj
+	{
+		public string Name { get; set; }
+
+		[IfSpecified]
+		public object PersonalNumber { get; set; }
+
+		[RequiredIf(When = "PersonalNumber != null")]
+		public object Gender { get; set; }
+	}
+
+	[AttributeUsage(AttributeTargets.Property)]
+	public class RequiredIf : Attribute
+	{
+		public string When { get; set; }
+	}
+
+
+
+	[AttributeUsage(AttributeTargets.Property)]
+	public class IfSpecified : Attribute
+	{
+		//public string Condition { get; set; }
+
+		public static void PerformCheck(object objectToCheck)
+		{
+			var properties = new List<PropertyInfo>();
+			var type = objectToCheck.GetType();
+
+			foreach (var prop in type.GetProperties())
+				if (prop.GetCustomAttributes(typeof(IfSpecified), true).Any())
+					properties.Add(prop);
+
+			foreach (var prop in properties)
+			{
+				object value = prop.GetValue(objectToCheck, null);
+
+				//Validation logic here
+				if (!String.IsNullOrEmpty(value as string))
+				{
+					var props = type.GetProperties().Where(p => IsDefined(p, typeof(RequiredIf)));
+
+					foreach (var propertyInfo in props.Where(propertyInfo => propertyInfo.GetValue(objectToCheck, null) == null))
+					{
+						throw new Exception(string.Format("Property value '{0}' of type '{1}' is required to be null if {2} is specified",
+							propertyInfo.Name, objectToCheck.GetType().FullName, prop.Name));
+					}
+				}
+			}
+		}
+	}
+}
+
