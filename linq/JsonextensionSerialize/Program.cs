e1888b8fbc7e88e9e7f8f894b827605b08f7b13b
stpriyanka <stpriyanka2011@gmail.com>
2017-03-08 16:08:02 +0100

    Updated  example using json extension data

--- linq/JsonextensionSerialize/Program.cs
+++ linq/JsonextensionSerialize/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading;
 using Newtonsoft.Json;
 
 namespace JsonextensionSerialize
@@ -8,34 +9,72 @@ namespace JsonextensionSerialize
 	{
 		static void Main(string[] args)
 		{
-			var other2 = new Dictionary<string, object> { { "key1", 1 }, { "key2", 2 }, { "key3", 3 } };
-
-			var p = new Person()
+			var p = new PersonContainer()
 			{
-				age = 12,
-				name = "somename",
-				other = other2
+				Age = 12,
+				Name = "A name",
+				Gender = "Female",
+				DOB = DateTime.UtcNow,
+				WorkStatus = new WorkStatus()
+				{
+					Address = "Regeringsgatan ",
+					EmployerName = "Test AB",
+					WorkingExperienceInYears = 2
+				}
 			};
 
 
 			var personS = JsonConvert.SerializeObject(p);
+			Console.WriteLine("Serialized data: " + personS + "\n\n\n");
 
-			Console.WriteLine(personS);
+			Console.WriteLine("Attempting to deserialize data.....");
+			Thread.Sleep(1000);
+
+			var resultUsingJsonExtension = JsonConvert.DeserializeObject<CustomizedPerson>(personS);
+			
+			Console.WriteLine("Age :" + resultUsingJsonExtension.Age);
+			if (resultUsingJsonExtension.Other != null)
+			{
+				Console.WriteLine("Other :");
+				foreach (var x in resultUsingJsonExtension.Other)
+				{
+					Console.WriteLine(x);
+
+				}
+			}
+			Console.WriteLine("Finish !");
 			Console.ReadLine();
 		}
 	}
-	class Person : Base
+	class CustomizedPerson
 	{
-		public int age;
+		public int Age { get; set; }
 
 		[JsonExtensionData]
-		public IDictionary<string, object> other;
+		public IDictionary<string, object> Other { get; set; }
+
 	}
 
-	abstract class Base
+	public class PersonContainer
 	{
-		public string name;
+		public string Name { get; set; }
 
+		public int Age { get; set; }
+
+		public DateTime DOB { get; set; }
+
+		public string Gender { get; set; }
+
+		public WorkStatus WorkStatus { get; set; }
+
+	}
+
+	public class WorkStatus
+	{
+
+		public string EmployerName { get; set; }
+		public string Address { get; set; }
+		public int WorkingExperienceInYears { get; set; }
 	}
 
 }
